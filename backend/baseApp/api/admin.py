530fbfa72ed60ea294b4f9b98a59c850e4c4650a
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-09-08 16:32:59 +0530

    main

--- backend/baseApp/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Students
-
-# Register your models here.
-@admin.register(Students)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username', 'email']
-
